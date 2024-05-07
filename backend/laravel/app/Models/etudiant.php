ac92f352792d3135d4fc4f871497841d40f01a5a
elsonwets <elsonwets@gmail.com>
2024-05-07 17:29:16 +0200

    Configuration of laravel passport

---
+++ backend/laravel/app/Models/etudiant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class etudiant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'dateNaissance',
+        'email',
+        'image',
+        'adresse',
+        'sexe'
+    ];
+}
