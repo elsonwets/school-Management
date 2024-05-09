2cd9450926d28c7db5e421a71a0e7db3d0323ae3
DialloHabib77 <justcodeur88@gmail.com>
2024-05-09 23:55:25 +0000

    Modification des routes api

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -28,71 +28,71 @@ Route::delete('etudiants/{etudiant}', [EtudiantController::class,'delete']);
 
 
 //Route Professeur
-Route::get('etudiants', [ProfesseurController::class,'index']);
-Route::get('etudiants/{etudiant}', [ProfesseurController::class,'show']);
-Route::post('etudiants', [ProfesseurController::class,'store']);
-Route::put('etudiants/{etudiant}', [ProfesseurController::class,'update']);
-Route::delete('etudiants/{etudiant}', [ProfesseurController::class,'delete']);
+Route::get('professeurs', [ProfesseurController::class,'index']);
+Route::get('professeurs/{professeur}', [ProfesseurController::class,'show']);
+Route::post('professeurs', [ProfesseurController::class,'store']);
+Route::put('professeurs/{professeur}', [ProfesseurController::class,'update']);
+Route::delete('professeurs/{professeur}', [ProfesseurController::class,'delete']);
 
 //Route Tuteur
-Route::get('etudiants', [TuteurController::class,'index']);
-Route::get('etudiants/{etudiant}', [ProfesseurController::class,'show']);
-Route::post('etudiants', [ProfesseurController::class,'store']);
-Route::put('etudiants/{etudiant}', [ProfesseurController::class,'update']);
-Route::delete('etudiants/{etudiant}', [ProfesseurController::class,'delete']);
+Route::get('tuteurs', [TuteurController::class,'index']);
+Route::get('tuteurs/{tuteur}', [ProfesseurController::class,'show']);
+Route::post('tuteurs', [ProfesseurController::class,'store']);
+Route::put('tuteurs/{tuteur}', [ProfesseurController::class,'update']);
+Route::delete('tuteurs/{tuteur}', [ProfesseurController::class,'delete']);
 
 //Route Inscription
-Route::get('etudiants', [InscriptionController::class,'index']);
-Route::get('etudiants/{etudiant}', [InscriptionController::class,'show']);
-Route::post('etudiants', [InscriptionController::class,'store']);
-Route::put('etudiants/{etudiant}', [InscriptionController::class,'update']);
-Route::delete('etudiants/{etudiant}', [InscriptionController::class,'delete']);
+Route::get('Inscriptions', [InscriptionController::class,'index']);
+Route::get('Inscriptions/{inscription}', [InscriptionController::class,'show']);
+Route::post('Inscriptions', [InscriptionController::class,'store']);
+Route::put('Inscriptions/{inscription}', [InscriptionController::class,'update']);
+Route::delete('Inscriptions/{inscription}', [InscriptionController::class,'delete']);
 
 //Route Matiere
-Route::get('etudiants', [MatiereController::class,'index']);
-Route::get('etudiants/{etudiant}', [MatiereController::class,'show']);
-Route::post('etudiants', [MatiereController::class,'store']);
-Route::put('etudiants/{etudiant}', [MatiereController::class,'update']);
-Route::delete('etudiants/{etudiant}', [MatiereController::class,'delete']);
+Route::get('matieres', [MatiereController::class,'index']);
+Route::get('matieres/{etudiant}', [MatiereController::class,'show']);
+Route::post('matieres', [MatiereController::class,'store']);
+Route::put('matieres/{matiere}', [MatiereController::class,'update']);
+Route::delete('matieres/{matiere}', [MatiereController::class,'delete']);
 
 //Route Message
-Route::get('etudiants', [MessageController::class,'index']);
-Route::get('etudiants/{etudiant}', [MessageController::class,'show']);
-Route::post('etudiants', [MessageController::class,'store']);
-Route::put('etudiants/{etudiant}', [MessageController::class,'update']);
-Route::delete('etudiants/{etudiant}', [MessageController::class,'delete']);
+Route::get('messages', [MessageController::class,'index']);
+Route::get('messages/{message}', [MessageController::class,'show']);
+Route::post('messages', [MessageController::class,'store']);
+Route::put('messages/{message}', [MessageController::class,'update']);
+Route::delete('messages/{message}', [MessageController::class,'delete']);
 
 //Route Note
-Route::get('etudiants', [NoteController::class,'index']);
-Route::get('etudiants/{etudiant}', [NoteController::class,'show']);
-Route::post('etudiants', [NoteController::class,'store']);
-Route::put('etudiants/{etudiant}', [NoteController::class,'update']);
-Route::delete('etudiants/{etudiant}', [NoteController::class,'delete']);
+Route::get('notes', [NoteController::class,'index']);
+Route::get('notes/{note}', [NoteController::class,'show']);
+Route::post('notes', [NoteController::class,'store']);
+Route::put('notes/{note}', [NoteController::class,'update']);
+Route::delete('notes/{note}', [NoteController::class,'delete']);
 
 //Route Paiement
-Route::get('etudiants', [PaiementController::class,'index']);
-Route::get('etudiants/{etudiant}', [PaiementController::class,'show']);
-Route::post('etudiants', [PaiementController::class,'store']);
-Route::put('etudiants/{etudiant}', [PaiementController::class,'update']);
-Route::delete('etudiants/{etudiant}', [PaiementController::class,'delete']);
+Route::get('paiements', [PaiementController::class,'index']);
+Route::get('paiements/{paiement}', [PaiementController::class,'show']);
+Route::post('paiements', [PaiementController::class,'store']);
+Route::put('paiements/{paiement}', [PaiementController::class,'update']);
+Route::delete('paiements/{paiement}', [PaiementController::class,'delete']);
 
 // Route Document Academique
-Route::get('etudiants', [DocumentAcademiqueController::class,'index']);
-Route::get('etudiants/{etudiant}', [DocumentAcademiqueController::class,'show']);
-Route::post('etudiants', [DocumentAcademiqueController::class,'store']);
-Route::put('etudiants/{etudiant}', [DocumentAcademiqueController::class,'update']);
-Route::delete('etudiants/{etudiant}', [DocumentAcademiqueController::class,'delete']);
+Route::get('documents', [DocumentAcademiqueController::class,'index']);
+Route::get('documents/{document}', [DocumentAcademiqueController::class,'show']);
+Route::post('documents', [DocumentAcademiqueController::class,'store']);
+Route::put('documents/{document}', [DocumentAcademiqueController::class,'update']);
+Route::delete('documents/{document}', [DocumentAcademiqueController::class,'delete']);
 
 //Route Classe
-Route::get('etudiants', [ClasseController::class,'index']);
-Route::get('etudiants/{etudiant}', [ClasseController::class,'show']);
-Route::post('etudiants', [ClasseController::class,'store']);
-Route::put('etudiants/{etudiant}', [ClasseController::class,'update']);
-Route::delete('etudiants/{etudiant}', [ClasseController::class,'delete']);
+Route::get('classes', [ClasseController::class,'index']);
+Route::get('classes/{classe}', [ClasseController::class,'show']);
+Route::post('classes', [ClasseController::class,'store']);
+Route::put('classes/{classe}', [ClasseController::class,'update']);
+Route::delete('classes/{classe}', [ClasseController::class,'delete']);
 
 //Route Annee Scolaire
-Route::get('etudiants', [AnneeScolaireController::class,'index']);
-Route::get('etudiants/{etudiant}', [AnneeScolaireController::class,'show']);
-Route::post('etudiants', [AnneeScolaireController::class,'store']);
-Route::put('etudiants/{etudiant}', [AnneeScolaireController::class,'update']);
-Route::delete('etudiants/{etudiant}', [AnneeScolaireController::class,'delete']);
+Route::get('annees', [AnneeScolaireController::class,'index']);
+Route::get('annees/{annee}', [AnneeScolaireController::class,'show']);
+Route::post('annees', [AnneeScolaireController::class,'store']);
+Route::put('annees/{annee}', [AnneeScolaireController::class,'update']);
+Route::delete('annees/{annee}', [AnneeScolaireController::class,'delete']);
